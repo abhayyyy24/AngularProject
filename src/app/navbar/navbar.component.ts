7515d5a82972f16466cb98a6bb914a546a6ae9d3
abhayyyy24 <pawar2abhay1910@gmail.com>
2025-04-19 01:05:59 +0530

    navbar completed

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
