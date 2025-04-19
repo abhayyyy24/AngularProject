aa26ce01835d016c26d4735056414ae6af9bd17e
abhayyyy24 <pawar2abhay1910@gmail.com>
2025-04-20 00:10:09 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from "./footer/footer.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
