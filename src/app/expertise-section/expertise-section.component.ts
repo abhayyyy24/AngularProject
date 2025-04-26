508e3ac9e57e305227f733c6cb1246f52dfc7cfd
abhayyyy24 <pawar2abhay1910@gmail.com>
2025-04-26 14:28:04 +0530

    initial commit

--- src/app/expertise-section/expertise-section.component.ts
+++ src/app/expertise-section/expertise-section.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'expertise-section',
-  imports: [CardComponent],
+  imports: [CardComponent,CommonModule],
   templateUrl: './expertise-section.component.html',
   styleUrl: './expertise-section.component.css'
 })
 export class ExpertiseSectionComponent {
-title: any;
+
+  expertiseCards = [
+    {
+      imageUrl: './webdev.png',
+      title: 'Web Development',
+      description: 'Building responsive and robust web applications.'
+    },
+    {
+      imageUrl: './mobileapp.png',
+      title: 'Mobile Apps',
+      description: 'Creating intuitive mobile experiences for iOS and Android.'
+    },
+    {
+      imageUrl: './desktop.png',
+      title: 'Desktop Solutions',
+      description: 'Developing powerful desktop applications.'
+    },
+    {
+      imageUrl: './ai.png',
+      title: 'Ai Integration',
+      description: 'Enhancing your business capabilities with AI.'
+    }
+  ];
 
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
-
+  scrolltoexpertise() {
+    const element = document.getElementById('expertise-section');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
